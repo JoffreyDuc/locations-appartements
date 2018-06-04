5e85a795d609d3e5b8afaf16554c21bb57c83493
JoffreyDuc <joffreyduc@gmail.com>
2018-06-05 00:22:35 +0200

    Fonctionnalité footer

--- index.php
+++ index.php
@@ -1,49 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<title>Appartements P. Revel</title>
-	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
-	<meta charset="utf-8">
+<?php ob_start(); ?>
 
-	<!-- Responsive design -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-
-	<!-- jQuery -->
-	<script src="lib/jquery/jquery-3.3.1.min.js"></script>
-	
-	<!-- Materialize -->
-	<link rel="stylesheet" href="lib/materialize/css/materialize.min.css">
-	<script src="lib/materialize/js/materialize.min.js"></script>
-
-	<!-- Styles -->
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-	<script src="js/main.js" type="text/javascript"></script>
-
-	<!-- Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Georgia" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-
-	<!-- Material Icons -->
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-</head>
-<body>
-	
-	<!-- Div contenant le background de la page -->
-	<?php include "background.html"; ?>
-
-	<!-- Menu -->
-	<?php include "menu.html"; ?>
-
-	<!-- Contenu -->
-	<div id="contenu" class="row">
-		<div class="col l8 offset-l2">
-			<h1 id="titre" class="white-text center">Appartements à louer sur le Bourget-du-Lac</h1>
-		</div>
+<div id="contenu" class="row">
+	<div class="col l8 offset-l2">
+		<h1 id="titre" class="white-text center">Appartements à louer sur le Bourget-du-Lac</h1>
 	</div>
+</div>
 
-	<!-- Pied de page -->
-	<?php include "footer.html"; ?>
-
-</body>
-</html>
+<?php $contenu = ob_get_clean(); ?>
+<?php require_once 'template.php'; ?>
---
+++ template.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>Appartements P. Revel</title>
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.png" />
+	<meta charset="utf-8">
+
+	<!-- Responsive design -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<!-- Materialize -->
+	<link rel="stylesheet" href="lib/materialize/css/materialize.min.css">
+
+	<!-- Styles -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<!-- Material Icons -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+</head>
+<body>
+
+	<!-- Menu -->
+	<?php include "menu.html"; ?>
+
+	<!-- Contenu -->
+	<main>
+		<?= $contenu; ?>
+	</main>
+
+	<!-- Pied de page -->
+	<?php include "footer.html"; ?>
+
+	<script src="lib/jquery/jquery-3.3.1.min.js"></script>
+	<script src="lib/materialize/js/materialize.min.js"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+
+</body>
+</html>
